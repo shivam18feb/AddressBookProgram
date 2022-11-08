884a129dcd4d79c1aa3ec7f102979666c30e3c2c
unknown <shivam18feb@gmail.com>
2022-11-08 16:30:02 +0530

    EDITcontact

--- src/com/bl/addressbook/AddressBookMain.java
+++ src/com/bl/addressbook/AddressBookMain.java
@@ -3,36 +3,41 @@ import java.util.*;
 public class AddressBookMain{
 
     public static void main(String[] args){
-    		String firstName, lastName, address, city, state, email;
-            int zip;
-            long phoneNo;
 
-            Scanner sc= new Scanner(System.in);
+        String firstName, lastName, address, city, state, email;
+        int zip;
+        long phoneNo;
+        //array to store object of type AddressBook
+        AddressBook[] contactArr = new AddressBook[2];
+
+        Scanner sc= new Scanner(System.in);
+        for(int i=0; i<contactArr.length; i++){
+           
+            //create new object in each iteration
             AddressBook contact = new AddressBook();
-            System.out.print("Enter First Name  : ");  
-            firstName = sc.next();
+
+            System.out.print("\nEnter First Name  : ");  
+            firstName = sc.nextLine();
             contact.setFirstName(firstName);
 
             System.out.print("\nEnter Last Name  : ");  
             lastName = sc.nextLine();
-            contact.setFirstName(lastName);
+            contact.setLastName(lastName);
 
             System.out.print("\nEnter E-mail  : ");
-            email = sc.next();
+            email = sc.nextLine();
             contact.setEmail(email);
 
             System.out.print("\nEnter Address  : ");  
-            address = sc.next(); 
+            address = sc.nextLine(); 
             contact.setAddress(address);
 
             System.out.print("\nEnter City  : ");  
-            city = sc.next(); 
+            city = sc.nextLine(); 
             contact.setCity(city);
 
-            
-
             System.out.print("\nEnter State  : ");  
-            state = sc.next();
+            state = sc.nextLine();
             contact.setState(state);
 
             System.out.print("\nEnter Phone Number  : ");   
@@ -43,15 +48,20 @@ public class AddressBookMain{
             zip = sc.nextInt(); 
             contact.setZip(zip);
 
-            sc.close();
-
-            System.out.print("\nFirst Name  : " +  contact.getFirstName());
-            System.out.print("\nLast Name   : " +  contact.getLastName());
-            System.out.print("\nAddress     : " +  contact.getAddress());
-            System.out.print("\nCity        : " +  contact.getCity());
-            System.out.print("\nState       : " +  contact.getState());
-            System.out.print("\nPone Number : " +  contact.getPhoneNo());
-            System.out.print("\nE-mail      : " +  contact.getEmail());
-            System.out.print("\nZip         : " +  contact.getZip());
+            sc.nextLine();
+            
+            contactArr[i] = contact;
         }
+        System.out.print("\nDo you want to edit? press Y / N : ");
+        char editOption = sc.next().charAt(0);
+        AddressBook person = new AddressBook();
+        if(editOption == 'Y'){
+            person.editContact(contactArr);
+        }
+        for(int i=0; i<contactArr.length; i++){
+
+           contactArr[i].showContact(contactArr[i]);
+        }
+    sc.close();
     }
+}
